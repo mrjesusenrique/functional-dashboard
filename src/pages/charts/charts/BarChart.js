c4221c9c7d6c2394d0d5f4d3544c5041396e4ce5
Jesús Casañas <jesusenrique9773@gmail.com>
2022-02-14 21:54:03 -0300

    prueba pantalla completa

--- src/pages/charts/charts/BarChart.js
+++ src/pages/charts/charts/BarChart.js
@@ -12,24 +12,35 @@ import {
 } from "recharts";
 
 const SimpleBarChart = (props) => {
+  const PantallaCompleta = () => {
+    let elemento = document.getElementById("grafico");
+    console.log(elemento);
+    if (elemento != null) {
+      elemento.requestFullscreen()
+    }
+  };
+
   return (
-    <ResponsiveContainer height={300} width="100%">
-      <BarChart
-        width={600}
-        height={300}
-        data={props.data}
-        margin={{ top: 20, left: -10 }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis yAxisId="left" orientation="left" stroke="#f3c363" />
-        <YAxis yAxisId="right" orientation="right" stroke="#eb3349" />
-        <Tooltip />
-        <Legend />
-        <Bar yAxisId="left" dataKey="pv" fill="#f3c363" />
-        <Bar yAxisId="right" dataKey="uv" fill="#eb3349" />
-      </BarChart>
-    </ResponsiveContainer>
+    <>
+      <button onClick={PantallaCompleta}>Pantalla Completa</button>
+      <ResponsiveContainer height={300} width="100%" id="grafico">
+        <BarChart
+          width={600}
+          height={300}
+          data={props.data}
+          margin={{ top: 20, left: -10 }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis yAxisId="left" orientation="left" stroke="#f3c363" />
+          <YAxis yAxisId="right" orientation="right" stroke="#eb3349" />
+          <Tooltip />
+          <Legend />
+          <Bar yAxisId="left" dataKey="pv" fill="#f3c363" />
+          <Bar yAxisId="right" dataKey="uv" fill="#eb3349" />
+        </BarChart>
+      </ResponsiveContainer>
+    </>
   );
 };
 
